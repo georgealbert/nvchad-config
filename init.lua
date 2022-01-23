b626605fc348f5ca50724400ce50815189106faa
georgealbert <georgealbert@qq.com>
2022-01-23 17:43:20 +0800

    squash! update readme.

--- init.lua
+++ init.lua
@@ -6,33 +6,21 @@
 -- MAPPINGS
 local map = require("core.utils").map
 
-map("n", "<leader>cc", ":Telescope <CR>")
+ffip = {} -- Find file in project. global variable, module name.
 
--- map("n", "<leader>bb", ":Telescope buffers<CR>")
--- map("n", "<leader>bk", ":lua require('core.utils').close_buffer() <CR>") -- close  buffer
-
--- map("n", "<C-c>n", ":Telescope buffers<CR>")
-
-map("n", "<C-x><C-s>", ":w <CR>")
-
-map("n", "<C-c><C-/>", ":lua require('Comment.api').toggle_current_linewise()<CR>")
-map("v", "<C-c><C-/>", ":lua require('Comment.api').toggle_linewise_op(vim.fn.visualmode())<CR>")
-
-function find_file_in_dir(opts)
+function ffip.find_file_in_dir(opts)
   opts = opts or {}
   -- opts.cwd = vim.fn.systemlist("git rev-parse --show-toplevel")[1]
   opts.cwd = vim.fn.expand('%:p:h')
   require'telescope.builtin'.find_files(opts)
 end
 
-map("n", "<leader>fd", ":lua find_file_in_dir()<CR>")
-
-function find_file_in_project(opts)
+function ffip.find_file_in_project(opts)
   opts = opts or {}
 
-  local lspUtil = require("lspconfig.util")
+  local util = require("lspconfig.util")
   local current_dir = vim.fn.expand('%:p:h')
-  local root_dir = lspUtil.find_git_ancestor(current_dir)
+  local root_dir = util.find_git_ancestor(current_dir)
 
   if not root_dir then
     print("Could not find the project root. Create a git repository there fisrt. Dir: " .. current_dir)
@@ -44,33 +32,22 @@ function find_file_in_project(opts)
   require'telescope.builtin'.find_files(opts)
 end
 
-map("n", "<leader>fp", ":lua find_file_in_project()<CR>")
-
 -- live_grep
 -- utils.buffer_dir()
-function grep_file_in_dir(opts)
+function ffip.grep_file_in_dir(opts)
   opts = opts or {}
   -- opts.cwd = vim.fn.systemlist("git rev-parse --show-toplevel")[1]
   opts.cwd = vim.fn.expand('%:p:h')
   require'telescope.builtin'.live_grep(opts)
 end
 
--- map("n", "<leader>gf", ":lua grep_file_in_dir()<CR>")
-
-map("n", "<D-f>", ":lua grep_file_in_dir()<CR>")
--- map("n", "<D-r>", ":lua grep_file_in_dir()<CR>")
--- map("n", "<M-f>f", ":lua grep_file_in_dir()<CR>")
--- map("n", "<A-f>f", ":lua grep_file_in_dir()<CR>")
-
--- map("n", "<leader>fa", ":lua require('telescope.builtin').find_files(vim.fn.expand('%:p:h'))<CR>")
-
 -- https://github.com/nvim-telescope/telescope.nvim/issues/592
 -- https://github.com/nvim-telescope/telescope.nvim/issues/502
-function grep_file_in_project(opts)
+function ffip.grep_file_in_project(opts)
   opts = opts or {}
-  local lspUtil = require("lspconfig.util")
+  local util = require("lspconfig.util")
   local current_dir = vim.fn.expand('%:p:h')
-  local root_dir = lspUtil.find_git_ancestor(current_dir)
+  local root_dir = util.find_git_ancestor(current_dir)
   -- print("buffer: " .. vim.fn.expand('%:p:h'))
   -- print("root: " .. lspPlugin.find_git_ancestor(vim.fn.expand('%:p:h')))
   -- print("root: " .. lspPlugin.find_git_ancestor("/Users/albert/.config/nvim/lua/custom/init.lua"))
@@ -85,7 +62,30 @@ function grep_file_in_project(opts)
   require'telescope.builtin'.live_grep(opts)
 end
 
-map("n", "<D-r>", ":lua grep_file_in_project()<CR>")
+--
+-- key mapping
+--
+map("n", "<leader>cc", ":Telescope <CR>")
+
+-- map("n", "<leader>bb", ":Telescope buffers<CR>")
+-- map("n", "<leader>bk", ":lua require('core.utils').close_buffer() <CR>") -- close  buffer
+
+-- map("n", "<C-c>n", ":Telescope buffers<CR>")
+
+map("n", "<C-x><C-s>", ":w <CR>")
+
+map("n", "<C-c><C-/>", ":lua require('Comment.api').toggle_current_linewise()<CR>")
+map("v", "<C-c><C-/>", ":lua require('Comment.api').toggle_linewise_op(vim.fn.visualmode())<CR>")
+
+
+map("n", "<leader>fd", ":lua ffip.find_file_in_dir()<CR>")
+map("n", "<leader>fp", ":lua ffip.find_file_in_project()<CR>")
+map("n", "<D-f>", ":lua ffip.grep_file_in_dir()<CR>")
+map("n", "<D-r>", ":lua ffip.grep_file_in_project()<CR>")
+-- map("n", "<D-r>", ":lua grep_file_in_dir()<CR>")
+-- map("n", "<M-f>f", ":lua grep_file_in_dir()<CR>")
+-- map("n", "<A-f>f", ":lua grep_file_in_dir()<CR>")
+-- map("n", "<leader>fa", ":lua require('telescope.builtin').find_files(vim.fn.expand('%:p:h'))<CR>")
 
 -- map("n", "<leader>q", ":q <CR>")
 -- NOTE: the 4th argument in the map function can be a table i.e options but its most likely un-needed so dont worry about it
@@ -111,6 +111,9 @@ customPlugins.add(function(use)
       }
 end)
 
+--
+-- which-key's key mapping.
+--
 local wk = require("which-key")
 
 wk.register({
