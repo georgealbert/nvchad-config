3fe9cc34c6ffb6b3320ca32fca86fc705f033909
georgealbert <georgealbert@qq.com>
2022-10-12 14:49:22 +0800

    新增mappings.lua

---
+++ mappings.lua
@@ -0,0 +1,36 @@
+-- lua/custom/mappings 
+local M = {}
+
+-- add this table only when you want to disable default keys
+M.disabled = {
+  n = {
+      ["<leader>b"] = "",
+      -- ["<C-c>"] = "",
+  }
+}
+
+M.telescope = {
+  n = {
+     ["<leader>bb"] = {"<cmd> Telescope buffers <CR>", "find buffers"},
+  },
+
+  i = {
+    -- more keys!
+  },
+}
+
+M.general = {
+   -- stuff
+   n = {
+      -- ["<C-c>n"] = {"<cmd> Telescope Buffers <CR>", "Open Buffers"},
+      ["<C-x><C-s>"] = {"<cmd> w <CR>", "save file"},
+      ["<leader>bk"] = {
+         function()
+            require("nvchad_ui.tabufline").close_buffer()
+         end,
+         "close buffer",
+      },
+   },
+}
+
+return M
