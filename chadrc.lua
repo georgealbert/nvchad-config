5500320c856801cee882a75e5a223cfca378c75b
georgealbert <georgealbert@qq.com>
2023-04-09 20:03:18 +0800

    增加statusline的分隔条风格显示，用默认的即可.

--- chadrc.lua
+++ chadrc.lua
@@ -10,6 +10,14 @@ M.ui = {
 
   hl_override = highlights.override,
   hl_add = highlights.add,
+
+  statusline = {
+    -- theme = "vscode_colored", -- default/vscode/vscode_colored/minimal
+
+    -- default/round/block/arrow (separators work only for "default" statusline theme;
+    -- round and block will work for the minimal theme only)
+    -- separator_style = "arrow",
+  },
 }
 
 M.plugins = "custom.plugins"
