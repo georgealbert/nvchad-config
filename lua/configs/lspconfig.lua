77696d039885c889104727cbef86e451d2cc278b
albert <georgealbert@qq.com>
2024-09-29 16:50:14 +0800

    lua/configs/lspconfig.lua: remove clangd

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,8 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "bashls", "basedpyright", "clangd", "jdtls" }
+-- local servers = { "bashls", "basedpyright", "clangd", "jdtls" }
+local servers = { "bashls", "basedpyright", "jdtls" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
